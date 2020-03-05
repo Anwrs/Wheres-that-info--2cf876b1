6e002f9cb16d5a65fff7e66b0d58b56431872bb4
Anouar ElGhazaoui <2080049@talnet.nl>
2020-03-05 15:39:25 +0100

    done

--- exercise.sql
+++ exercise.sql
@@ -1,4 +1,31 @@
--- CREATE DATABASE netland;
+SELECT title
+FROM series
+WHERE has_won_awards = 1;
 
-SELECT id, title, rating, description, has_won_awards, seasons, country, language
-FROM series;
+SELECT title
+FROM series
+WHERE rating >= 2.5;
+
+SELECT title
+FROM series
+WHERE country = 'NL' AND language = 'NL';
+
+SELECT title
+FROM series
+WHERE seasons <= 5;
+
+SELECT title
+FROM series
+ORDER BY rating DESC;
+
+SELECT title
+FROM series
+WHERE seasons < 3 OR seasons > 20;
+
+SELECT title
+FROM series
+WHERE title LIKE '%th%';
+
+SELECT title
+FROM series
+WHERE seasons != 3;
